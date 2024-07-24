1b682df99255c487920d1a8ead710b1a8f4f5f7e
Sadadi Assani <sadadi.assani@laposte.fr>
2024-07-24 12:42:03 +0200

    exo interface done

--- src/test/java/java17/ex01/Method_01_Test.java
+++ src/test/java/java17/ex01/Method_01_Test.java
@@ -19,6 +19,19 @@ public class Method_01_Test {
 
         // TODO créer une méthode int sumAge()
         // TODO Cette méthode retourne le résultat de l'addition des ages des personnes
+        
+        default int sumAge() {
+        	int somme = 0;
+            
+            for(Person p : findAll()) {
+            	somme+=p.getAge();
+           	 
+            }
+            return somme;
+        	
+        }
+        
+        
     }
     // end::IDao[]
 
@@ -48,7 +61,10 @@ public class Method_01_Test {
         DaoA daoA = new DaoA();
 
         // TODO invoquer la méthode sumAge pour que le test soit passant
-        int result = 0;
+        
+        
+        int result = daoA.sumAge() ;
+        
 
         assert result == 210;
     }
@@ -59,7 +75,7 @@ public class Method_01_Test {
         DaoB daoB = new DaoB();
 
         // TODO invoquer la méthode sumAge pour que le test soit passant
-        int result = 0;
+        int result = daoB.sumAge();
 
         assert result == 5050;
 
